b82bd9b163352c111f561082d503e0c60718c6bb
Michael Matasyants <michael.matasyanc@gmail.com>
2023-08-25 19:21:31 +0300

    Update display for admin

--- flower_shop/mainapp/admin.py
+++ flower_shop/mainapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from mainapp.models import User, Flower, Occasion, Bouqet, Order
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['tg_id', 'role']
+
+
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Occasion)
+class OccasionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bouqet)
+class BouqetAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
